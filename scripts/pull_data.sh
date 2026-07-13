feb27cf348892280bfac780fcc81bf218316b133
Paul <paul418.urquhart108@gmail.com>
2026-07-13 23:19:45 +0100

    change data source to AWS

--- scripts/pull_data.sh
+++ scripts/pull_data.sh
@@ -4,14 +4,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../.env"
 
 
-curl --request GET "$HOST/api/2.0/fs/files/Volumes/dow30/default/export/gold_index.csv" \
-  --header "Authorization: Bearer $TOKEN" \
-  --output $FILE_PATH/gold_dow_index.csv
-
-curl --request GET "$HOST/api/2.0/fs/files/Volumes/dow30/default/export/gold_top5.csv" \
-  --header "Authorization: Bearer $TOKEN" \
-  --output $FILE_PATH/gold_top5.csv
-
-curl --request GET "$HOST/api/2.0/fs/files/Volumes/dow30/default/export/gold_bottom5.csv" \
-  --header "Authorization: Bearer $TOKEN" \
-  --output $FILE_PATH/gold_bottom5.csv
+aws s3 sync \
+    s3://paul-dow30-data/export/dow_prod/ \
+    $FILE_PATH
