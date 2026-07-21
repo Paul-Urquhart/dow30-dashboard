acc4e5760bd525baf517bc8f054d9f4f5a0dd5e9
Paul <paul418.urquhart108@gmail.com>
2026-07-21 20:51:34 +0100

    update to pull data from AWS

--- scripts/pull_data.sh
+++ scripts/pull_data.sh
@@ -4,6 +4,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../.env"
 
 
-aws s3 sync \
-    s3://paul-dow30-data/export/dow_prod/ \
-    $FILE_PATH
+aws s3 cp \
+    s3://paul-dow30-data/export/dow_prod/gold_dow_index.csv \
+    $FILE_PATH/gold_dow_index.csv
+
+aws s3 cp \
+    s3://paul-dow30-data/export/dow_prod/gold_top5.csv \
+    $FILE_PATH/gold_top5.csv
+
+aws s3 cp \
+    s3://paul-dow30-data/export/dow_prod/gold_bottom5.csv \
+    $FILE_PATH/gold_bottom5.csv
